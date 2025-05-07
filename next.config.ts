9e418d43965e8b236b3d9b99694db79542bbb6a4
Joshua Mamangun <mamangunjoshua@gmail.com>
2025-05-07 19:29:26 +0800

    Add Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: 'export',
+  	basePath: '/practice-card-questions',
+  	assetPrefix: '/practice-card-questions/',
+	trailingSlash: true,
 };
 
 export default nextConfig;
